a616ce99bf1124c884c59415dd7163fdcc297fa6
Samuel <samuelvieiradasilva96@hotmail.com>
2020-10-29 16:50:32 -0300

    criei minha folha de estilo propria

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/style.css">
     <title>@yield('title')</title>
 </head>
 <body class="min-h-screen bg-platform-900">
