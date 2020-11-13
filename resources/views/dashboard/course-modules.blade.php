ae8563646558fd3be089d7afa6702fb720586b88
Samuel <samuelvieiradasilva96@hotmail.com>
2020-11-13 18:06:54 -0300

    pequena alteraçao no front de modulos do curso

--- resources/views/dashboard/course-modules.blade.php
+++ resources/views/dashboard/course-modules.blade.php
@@ -5,9 +5,8 @@
 @section('content')
     <div class="main-modules">
         <div class="modules-container">
-
             <div class="module-single">
-                <div style="width: 40px; background-color: #ff00ff; height: 100%; margin-right: 10px;">
+                <div style="width: 80px; background-color: #ff00ff; height: 100%;">
 
                 </div>
                 <div class="modules-options">
@@ -18,7 +17,7 @@
             </div>
 
             <div class="module-single">
-                <div style="width: 40px; background-color: #ff00ff; height: 100%; margin-right: 10px;">
+                <div style="width: 80px; background-color: #ff00ff; height: 100%;">
 
                 </div>
                 <div class="modules-options">
@@ -27,16 +26,33 @@
                     <a href="">Excluir modulo</a>
                 </div>
             </div>
-            
-            <div class="options">
-                <div class="save-module">
-                    <a href="">Salvar alterações</a>
-                </div>
 
-                <div class="new-module">
-                    <a href="">Adicionar novo módulo</a>
+            <div class="module-single">
+                <div style="width: 80px; background-color: #ff00ff; height: 100%;">
+
+                </div>
+                <div class="modules-options">
+                    <a href="">Exibir aulas</a>
+                    <a href="">Editar modulo</a>
+                    <a href="">Excluir modulo</a>
                 </div>
             </div>
+
+            <div class="module-single">
+                <div style="width: 80px; background-color: #ff00ff; height: 100%;">
+
+                </div>
+                <div class="modules-options">
+                    <a href="">Exibir aulas</a>
+                    <a href="">Editar modulo</a>
+                    <a href="">Excluir modulo</a>
+                </div>
+            </div>
+           
+            <div class="new-module">
+                <a href="">Adicionar novo módulo</a>
+            </div>
+
             
         </div>
     </div>
